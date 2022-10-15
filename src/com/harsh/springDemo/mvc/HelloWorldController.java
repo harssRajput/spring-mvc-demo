a7e67110f009e1d7ad7626dcb1ece0ae19e16d4e
Harsh Rajput <hrrajput20@gmail.com>
2022-10-15 16:37:37 +0530

    fetch form data by RequestParam annotation in /processFormV2 path

--- src/com/harsh/springDemo/mvc/HelloWorldController.java
+++ src/com/harsh/springDemo/mvc/HelloWorldController.java
@@ -1,10 +1,9 @@
 package com.harsh.springDemo.mvc;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -20,10 +19,10 @@ public class HelloWorldController {
 	}
 	
 	@RequestMapping("/processFormV2")
-	public String letsShoutDude(HttpServletRequest request, Model model) {
+	public String letsShoutDude(@RequestParam("studentName") String name, Model model) {
 		
 		//read the request param from HTML form
-		String name = request.getParameter("studentName");
+//		String name = request.getParameter("studentName");
 		
 		//covert to all caps
 		name = name.toUpperCase();
